83081bfb6913d4ada1c33d916332a8c93d35338f
Andre Buchanan <30668775+AndreBuchanan92@users.noreply.github.com>
2019-09-30 19:55:33 -0500

    upload with comments
    upload with comments

--- stack.cpp
+++ stack.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include<string.h>
 
+
 using namespace std;
+
 
 bool Stack::push(int x, string stringid) 
 { 
@@ -12,10 +14,10 @@ bool Stack::push(int x, string stringid)
     } 
     else {
     	newNode->id=x;
-    	newNode->data=stringid;
+    	newNode->data=stringid; //create struct to hold data 
     	
-    	stack[top++] = newNode; 
-        cout << stack[top-1]->id << " pushed into stack\n"; 
+    	stack[top++] = newNode; //increment stack counter 
+        cout << stack[top-1]->id << " pushed into stack\n"; //push pointer for the struct onto the stack
         return true; 
     }
 } 
@@ -27,10 +29,10 @@ bool Stack::pop(Data * dataPtr)
         return false; 
     } 
     else { 
-    	*dataPtr=*stack[top-1];
-    	free(stack[top-1]);
-    	top--;
-        return true; 
+    	*dataPtr=*stack[top-1]; //get data from the top of the stack
+    	free(stack[top-1]); //delete the allocated memory from the top of the stack
+    	top--; //decrement your stack counter
+        return true; //return the data to the caller 
     } 
 } 
 
@@ -41,18 +43,18 @@ bool Stack::peek(Data * dataPtr)
         return false; 
     } 
     else { 
-		*dataPtr=*stack[top-1];
-		return true; 
+		*dataPtr=*stack[top-1]; //read the data from the top of the stack without popping
+		return true;  //return data 
     } 
 } 
   
 bool Stack::isEmpty() 
 { 
     if (top	== 0) {
-    	return true;
+    	return true; //return true based on the count
 	}
     else {
-    	return false;
+    	return false; //return false based on the count 
 	}
 } 
 
@@ -64,8 +66,8 @@ Stack::Stack() {
 	top=0;
 	for(int i=0;i<STACK_SIZE;i++)
 	{
-		stack[i]=(Data *)malloc(sizeof(Data));
-	}
+		stack[i]=(Data *)malloc(sizeof(Data)); //return number of structs in the data. I used malloc to allocate memory block 
+	} 
 }
 Stack::~Stack() {
 }
